95816f6e359b0a5e9ef7934a5662109fe54b77d7
Nick Owens <mischief@offblast.org>
2026-05-29 23:24:54 -0700

    env: rewrite to properly handle command execution
    env was just printing the environment. Now handles the full
    POSIX interface: env [-i] [name=value...] command [args...]
    
    This is required for #!/usr/bin/env lua5.4 shebangs to work.

--- env/env.lua
+++ env/env.lua
@@ -1,8 +1,49 @@
 #!/usr/bin/env lua5.4
 -- SPDX-License-Identifier: ISC
-local unistd   = require("posix.unistd")
-local notposix = require("notposix")
+-- env - set environment and execute command, or print environment
+local unistd = require("posix.unistd")
+local stdlib = require("posix.stdlib")
 
-for _, entry in ipairs(notposix.environ()) do
-	unistd.write(1, entry .. "\n")
+local clear = false
+local i = 1
+
+-- Parse options
+while i <= #arg do
+	if arg[i] == "-i" or arg[i] == "-" then
+		clear = true
+	elseif arg[i] == "--" then
+		i = i + 1
+		break
+	elseif arg[i]:sub(1, 1) == "-" then
+		unistd.write(2, "env: invalid option -- '" .. arg[i]:sub(2) .. "'\n")
+		os.exit(125)
+	else
+		break
+	end
+	i = i + 1
 end
+
+-- Process NAME=VALUE pairs
+while i <= #arg and arg[i]:find("=") do
+	local name, val = arg[i]:match("^([^=]+)=(.*)")
+	if name then stdlib.setenv(name, val, true) end
+	i = i + 1
+end
+
+-- No command: print environment
+if i > #arg then
+	for k, v in pairs(stdlib.getenv()) do
+		if type(k) == "string" then
+			unistd.write(1, k .. "=" .. v .. "\n")
+		end
+	end
+	os.exit(0)
+end
+
+-- Execute command
+local cmd = arg[i]
+local cmd_args = {}
+for j = i + 1, #arg do cmd_args[#cmd_args + 1] = arg[j] end
+unistd.execp(cmd, cmd_args)
+unistd.write(2, "env: '" .. cmd .. "': No such file or directory\n")
+os.exit(127)

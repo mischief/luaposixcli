35290a7f8098f3fbd29b61e8c704501cbf935022
Nick Owens <mischief@offblast.org>
2026-05-31 12:14:05 -0700

    sleep: exit silently on SIGINT

--- sleep/sleep.lua
+++ sleep/sleep.lua
@@ -1,4 +1,6 @@
 #!/usr/bin/env lua5.4
 -- SPDX-License-Identifier: ISC
+local signal = require("posix.signal")
 local unistd = require("posix.unistd")
+signal.signal(signal.SIGINT, function() os.exit(130) end)
 unistd.sleep(math.floor(tonumber(arg[1]) or 0))
